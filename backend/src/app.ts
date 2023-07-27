6ee62fd457c3e09255cdf8d2703bb6a10ce95d59
jesus <jagonzalez954@soy.sena.edu.co>
2023-07-27 17:33:02 -0500

    creacion de rutas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,8 @@ const app = express();
 //settings
 app.set('port', 3000)
 
+
+
 //Starting the server
 app.listen(app.get('port'), ()=>{
     console.log(`Funcionandig, Server on port ${app.get('port')}`);
