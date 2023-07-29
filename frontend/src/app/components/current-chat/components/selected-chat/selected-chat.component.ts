952811e0dcdc492d6cf0f351e094013e076bea6a
count <santiagoosorio310@gmail.com>
2023-07-29 00:27:09 -0500

    feat: create current-chat component

---
+++ frontend/src/app/components/current-chat/components/selected-chat/selected-chat.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selected-chat',
+  templateUrl: './selected-chat.component.html',
+  styleUrls: ['./selected-chat.component.css']
+})
+export class SelectedChatComponent {
+
+}
---
+++ frontend/src/app/components/current-chat/current-chat.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-chat',
+  templateUrl: './current-chat.component.html',
+  styleUrls: ['./current-chat.component.css']
+})
+export class CurrentChatComponent {
+
+}
