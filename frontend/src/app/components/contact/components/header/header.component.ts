a624f1470ee32d6725f7d99635e2047bd300008f
count <santiagoosorio310@gmail.com>
2023-08-03 15:17:27 -0500

    feat: create contact header component

---
+++ frontend/src/app/components/contact/components/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+imagen:string="https://media.diariolasamericas.com/p/fe8db18fa8fb42e1d8f17ffc38e2018b/adjuntos/216/imagenes/000/214/0000214007/fotogaleria-las-actrices-mejor-pagadas-hollywood.jpg"
+}
