c84e04c6689c4084194e7959e376a6d823854bed
count <santiagoosorio310@gmail.com>
2023-08-03 15:18:16 -0500

    feat(changelog): update routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigInViewComponent } from './views/sig-in-view/sig-in-view.component';
 import { CurrentChatComponent } from './components/current-chat/current-chat.component';
+import { MainComponent } from './views/main/main.component';
 
 const routes: Routes = [
-  // {
+  // {w
   //   path: '',
   //   redirectTo: 'chat',
   //   pathMatch: 'full',
   // },
+  {path:'', component: MainComponent},
   {path:'signin', component: SigInViewComponent},
   {path:'signup', component: SigInViewComponent},
   {path:':room', component: CurrentChatComponent},
